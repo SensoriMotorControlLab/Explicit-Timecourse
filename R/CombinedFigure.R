f2bfd8ee40e89d8bb05ff3ba14f3e1d58b4a99e0
Elysa Eliopulos <elysa@EL-PF50BWNR.yorku.yorku.ca>
2025-04-16 13:00:09 -0400

    Created two functions that plot the exponential curves for aim deviation and reach deviation.

--- R/CombinedFigure.R
+++ R/CombinedFigure.R
@@ -46,3 +46,71 @@ plotAllReachOverlay <- function() {
          col = c( "mediumpurple", "seagreen3", "magenta", "orange", "cyan"), lty = 1, lwd = 2)
 }
 
+
+
+#plot reach dev exponentials
+
+plotExponentialReachOverlay <- function() {
+  timepoints <- 0:256
+  
+  params_60 <- c(lambda=0.006605567, N0=59.463737995)
+  params_50 <- c(lambda=0.005643455, N0=48.615362434)
+  params_40 <- c(lambda=0.005190853, N0=37.533714916)
+  params_30 <- c(lambda=0.007636479 , N0=29.624525083 )
+  params_20 <- c(lambda=0.01035161 , N0=17.40091762  )
+  
+  curve_60 <- params_60["N0"] * (1 - exp(-params_60["lambda"] * timepoints))
+  curve_50 <- params_50["N0"] * (1 - exp(-params_50["lambda"] * timepoints))
+  curve_40 <- params_40["N0"] * (1 - exp(-params_40["lambda"] * timepoints))
+  curve_30 <- params_30["N0"] * (1 - exp(-params_30["lambda"] * timepoints))
+  curve_20 <- params_20["N0"] * (1 - exp(-params_20["lambda"] * timepoints))
+  
+  plot(timepoints, curve_60, type="l", col="cyan", lwd=2, 
+       ylim=c(0, 60), 
+       xlab="Trial", ylab="Reach Deviation (°)", 
+       main="Exponential Learning Curves in Different Aiming Conditions")
+  
+  lines(timepoints, curve_50, col= "magenta", lwd=2)
+  lines(timepoints, curve_40, col="orange", lwd=2)
+  lines(timepoints, curve_30, col= "cornflowerblue", lwd=2)
+  lines(timepoints, curve_20, col= "limegreen", lwd=2)
+  
+  legend("topleft", legend=c("60°", "50°", "40°", "30°", "20°"), 
+         col=c("cyan", "magenta", "orange","cornflowerblue",'limegreen'), lty=1, lwd=2)
+  
+  
+}
+
+
+#plot aim dev exponentials
+
+plotExponentialAimOverlay <- function() {
+  timepoints <- 0:256
+  
+  params_60aim <- c(lambda=0.003549872, N0=48.987139824 )
+  params_50aim <- c(lambda=0.003647977, N0=24.259366886 )
+  params_40aim <- c(lambda=0.003168895, N0=20.519496217 )
+  params_30aim <- c(lambda=0.0036941, N0=17.6704411)
+  params_20aim <- c(lambda=0.003574507, N0=4.237782708 )
+  
+  curve_60aim <- params_60aim["N0"] * (1 - exp(-params_60aim["lambda"] * timepoints))
+  curve_50aim <- params_50aim["N0"] * (1 - exp(-params_50aim["lambda"] * timepoints))
+  curve_40aim <- params_40aim["N0"] * (1 - exp(-params_40aim["lambda"] * timepoints))
+  curve_30aim <- params_30aim["N0"] * (1 - exp(-params_30aim["lambda"] * timepoints))
+  curve_20aim <- params_20aim["N0"] * (1 - exp(-params_20aim["lambda"] * timepoints))
+  
+  plot(timepoints, curve_60aim, type="l", col="cyan", lwd=2, 
+       ylim=c(0, 60), 
+       xlab="Trial", ylab="Aim Deviation (°)", 
+       main="Exponential Learning Curves in Different Aiming Conditions")
+  
+  lines(timepoints, curve_50aim, col="magenta", lwd=2)
+  lines(timepoints, curve_40aim, col="orange", lwd=2)
+  lines(timepoints, curve_30aim, col="dodgerblue", lwd=2)
+  lines(timepoints, curve_20aim, col="limegreen", lwd=2)
+  
+  legend("topleft", legend=c("60°", "50°", "40°", "30°", "20°"), 
+         col=c("cyan", "magenta", "orange","dodgerblue",'limegreen'), lty=1, lwd=2)
+}
+
+
