c55583227c0ecb4a31503cb98ae42be794a40de3
Elysa Eliopulos <elysaeliopulos@elysas-mbp.home>
2025-10-30 16:31:15 -0400

    Fixed code

--- UnsupervisedK.R
+++ UnsupervisedK.R
@@ -29,30 +29,47 @@ block_features <- block_data %>%
   arrange(cutrial_no, .by_group = TRUE) %>%
   mutate(change_in_aim = aimdeviation_deg - lag(aimdeviation_deg)) %>%
   summarise(
-    mean_aim     = mean(aimdeviation_deg, na.rm = TRUE),
-    sd_aim       = sd(aimdeviation_deg, na.rm = TRUE),
-    mean_change  = mean(change_in_aim, na.rm = TRUE),
- -   .groups = "drop"
-  )
-
+    mean_aim = mean(aimdeviation_deg, na.rm = TRUE),
+    sd_aim = sd(aimdeviation_deg, na.rm = TRUE),
+    .groups = "drop"
+  ) %>%
+  group_by(participant_id) %>%
+  mutate(
+    block_of_change = which(mean_aim > 7 | mean_aim < -7)[1]
+  ) %>%
+  ungroup()
 
 block_wide <- block_features %>%
   pivot_wider(
     id_cols = participant_id,
     names_from = block,
-    values_from = c(mean_aim, sd_aim, mean_change),
-    names_glue = "{.value}_block_{block}"
+    values_from = c(mean_aim, sd_aim, block_of_change)
   )
 
+#theres some blocks with no changes, indicating stability so change NA to 0
+block_scaled <- block_wide %>%
+  select(-participant_id) %>%
+  mutate(across(everything(), ~ ifelse(is.na(.), 0, .))) %>%
+  select(where(~ sd(.) != 0)) %>%   # remove columns with zero variance
+  scale()
 
-block_scaled <- scale(block_wide %>% select(-participant_id))
 set.seed(123)
 km <- kmeans(block_scaled, centers = 3, nstart = 50)
 block_wide$cluster <- factor(km$cluster)
 
 
+strategy_data %>%
+  filter(trial_type.x == "rotated") %>%
+  left_join(block_wide %>% select(participant_id, cluster), by = "participant_id") %>%
+  ggplot(aes(x = trial_after_rot, y = aimdeviation_deg, group = participant_id)) +
+  geom_line(alpha = 0.6) +
+  facet_wrap(~cluster, ncol = 1) +
+  theme_minimal() +
+  labs(title = "Aiming Cluster: Block-level",
+       x = "Trial after rotation",
+       y = "Aiming deviation (deg)")
+
 #see clusters
-*
 
 participants_by_cluster <- block_wide %>%
   group_by(cluster) %>%
@@ -61,24 +78,4 @@ participants_by_cluster <- block_wide %>%
 list <-  participants_by_cluster %>%
   unnest(cols = participant_ids) %>%
   arrange(cluster)
-
-
-###plot raw aiming
-
-strategy_data_clustered <- block_data %>%
-  left_join(block_wide %>% select(participant_id, cluster), by = "participant_id") %>%
-  mutate(cluster = factor(cluster))  
-
-ggplot(strategy_data_clustered, 
-       aes(x = trial_after_rot, y = aimdeviation_deg, group = participant_id, color = cluster)) +
-  geom_line(alpha = 0.3, size = 0.6) +      
-  facet_wrap(~ cluster, ncol = 1) +          
-  theme_minimal(base_size = 13) +
-  coord_cartesian(ylim = c(-90, 90)) + 
-  scale_color_brewer(palette = "Set2") +    
-  labs(title = "Raw Trial-by-Trial Aiming per Cluster",
-       x = "Trial (after rotation onset)",
-       y = "Aiming Deviation (°)",
-       color = "Cluster") +
-  theme(panel.grid = element_blank())
 
