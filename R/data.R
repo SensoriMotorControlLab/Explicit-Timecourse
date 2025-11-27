ac58961c784eaf93214a8b649f704899c921a54b
Elysa Eliopulos <elysaeliopulos@elysas-mbp.home>
2025-11-26 22:17:05 -0500

    Updated functions for easier sourcing

--- R/data.R
+++ R/data.R
@@ -3,11 +3,12 @@
 library(osfr)
 
 #find project 
-project <- osf_retrieve_node("6g3h7")
-files <- osf_ls_files(project)
 
-
-osf_download(files, path = "data/", conflicts = "overwrite")
+downloadData <- function() {
+  project <- osf_retrieve_node("6g3h7")
+  files <- osf_ls_files(project)
+  osf_download(files, path = "data/", conflicts = "overwrite")
+}
 
 getData <- function() {
   
